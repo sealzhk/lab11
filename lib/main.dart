c93fd6df1553cd452495aa1c028e0391908e114e
sealzhk <sealzhk@users.noreply.github.com>
2021-04-26 21:59:12 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+enum ColorEvent { event_red, event_green }
+
+class ColorBloc {
+  Color _color = Colors.red;
+
+  final _inputEventController = StreamController<ColorEvent>();
+  StreamSink<ColorEvent> get inputEventSink => _inputEventController.sink;
+
+  final _outputStateController = StreamController<Color>();
+  Stream<Color> get outputStateStream => _outputStateController.stream;
+
+  void _mapEventToState(ColorEvent event) {
+    if (event == ColorEvent.event_red)
+      _color = Colors.red;
+    else if (event == ColorEvent.event_green)
+      _color = Colors.green;
+    else
+      throw Exception('Wrong Event Type');
+
+    _outputStateController.sink.add(_color);
+  }
+
+  ColorBloc() {
+    _inputEventController.stream.listen(_mapEventToState);
+  }
+
+  void dispose() {
+    _inputEventController.close();
+    _outputStateController.close();
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ColorBloc _bloc = ColorBloc();
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BLoC with Stream'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: _bloc.outputStateStream,
+          initialData: Colors.red,
+          builder: (context, snapshot) {
+            return AnimatedContainer(
+              height: 100,
+              width: 100,
+              color: snapshot.data,
+              duration: Duration(milliseconds: 500),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            backgroundColor: Colors.red,
+            onPressed: () {
+              _bloc.inputEventSink.add(ColorEvent.event_red);
+            },
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              _bloc.inputEventSink.add(ColorEvent.event_green);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
